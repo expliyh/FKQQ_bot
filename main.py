ad5a2fb747323e7a06086e86628367dcb83cfe4d
expliyh <54161210+expliyh@users.noreply.github.com>
2022-06-01 22:30:14 +0800

    update

--- main.py
+++ main.py
@@ -4,18 +4,53 @@ import requests
 import json
 import sys
 import random
+import asyncio
 
 if __name__ == '__main__':
-    print("Program started!")
-    database_name = os.environ['database']
-    username = os.environ['username']
-    if username == 'expli':
-        username = os.environ['database']
-    password = os.environ['password']
-    address = os.environ['address']
-    print("Env finished!")
+    # print("Program started!")
+    # database_name = os.environ['database']
+    # username = os.environ['username']
+    # if username == 'expli':
+    #    username = os.environ['database']
+    # password = os.environ['password']
+    # address = os.environ['address']
+    # print("Env finished!")
     # pixiv_name = os.environ['pixiv_username']
     # pixiv_pass = os.environ['pixiv_password']
     # refresh = os.environ['pixiv_refresh']
-    db = database.Database(database_name, username, address, password)
-    print("Db connected!")
+    # db = database.Database(database_name, username, address, password)
+    # print("Db connected!")
+
+    from botoy import *
+    from botoy.decorators import *
+
+    bot = AsyncBotoy(
+        host=jconfig.host,
+        port=jconfig.port,
+        log=True,
+        # log=False,
+        log_file=True,
+        use_plugins=True,
+    )
+
+
+    @from_these_groups(760088301)
+    @on_regexp(r'\d{17}[\d|x]|\d{15}')
+    def undo_id_num(ctx: GroupMsg):
+        action1 = Action()
+        action1.revoke(ctx)
+        S.text("请不要发送含隐私内容的消息！")
+
+
+    @from_these_groups(100867704, 760088301)
+    @equal_content("/test")
+    def test(ctx: GroupMsg):
+        Action(ctx.CurrentQQ).revokeGroupMsg(
+            group=ctx.FromGroupId,
+            msgSeq=ctx.MsgSeq,
+            msgRandom=ctx.MsgRandom,
+        )
+        S.text("ok")
+
+
+    asyncio.run(bot.run())
---
+++ plugins/bot_bili_vtbs/__init__.py
@@ -0,0 +1,43 @@
+"""查成分，查看B站关注虚拟主播成分
+发送：查成分+B站ID
+"""
+import json
+from pathlib import Path
+
+from botoy import S
+from botoy.decorators import ignore_botself, on_regexp, from_these_groups
+import httpx
+
+vtb_path = Path(__file__).absolute().parent / 'vtbs.json'
+
+with open(vtb_path) as f:
+    vtbs_data = json.load(f)
+
+
+@ignore_botself
+@from_these_groups(100867704, 760088301)
+@on_regexp(r"查成分(\d+)")
+def receive_group_msg(ctx):
+    resp = httpx.get(
+        f'https://account.bilibili.com/api/member/getCardByMid?mid={ctx._match.group(1)}',
+        timeout=10,
+    )
+    ret = resp.json()
+    if ret['code'] != 0:
+        return
+
+    card = ret['card']
+
+    vtbs = []
+    for mid in card['attentions']:
+        mid = str(mid)
+        if mid in vtbs_data:
+            vtb = vtbs_data[mid]
+            vtbs.append(f"{vtb['uname']}({mid})")
+
+    vtb_count = len(vtbs)
+    vtb_msg = '、\n'.join(vtbs)
+
+    S.image(
+        card['face'], f"{card['name']}({card['mid']})关注了 {vtb_count} 个vtb:\n{vtb_msg}"
+    )
