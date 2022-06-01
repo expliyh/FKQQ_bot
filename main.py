4878cb758b626cf2a1368555c79bc22830974067
expliyh <54161210+expliyh@users.noreply.github.com>
2022-06-01 20:08:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,3 +14,8 @@ if __name__ == '__main__':
     password = os.environ['password']
     address = os.environ['address']
     print("Env finished!")
+    # pixiv_name = os.environ['pixiv_username']
+    # pixiv_pass = os.environ['pixiv_password']
+    # refresh = os.environ['pixiv_refresh']
+    db = database.Database(database_name, username, address, password)
+    print("Db connected!")
